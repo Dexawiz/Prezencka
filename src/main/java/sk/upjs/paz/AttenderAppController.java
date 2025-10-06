5eaa8e07ef25ec94ced265cb2ebe02be842c4860
dexawiz <nazarp118@gmail.com>
2025-10-06 12:37:59 +0200

    generate random

---
+++ src/main/java/sk/upjs/paz/AttenderAppController.java
@@ -0,0 +1,21 @@
+package sk.upjs.paz;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class AttenderAppController {
+
+    @FXML
+    private Button generujButton;
+
+    @FXML
+    private Label vysledokLabel;
+
+    @FXML
+    void generujButtonAction(ActionEvent event) {
+        vysledokLabel.setText(Math.random() + "");
+    }
+
+}
