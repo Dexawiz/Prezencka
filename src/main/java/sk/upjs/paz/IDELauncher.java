ebf2f7b0c234b3d91d1692fd2e75d5056e5ff18a
dexawiz <nazarp118@gmail.com>
2025-10-06 12:20:17 +0200

    added javafx and first scene

---
+++ src/main/java/sk/upjs/paz/IDELauncher.java
@@ -0,0 +1,9 @@
+package sk.upjs.paz;
+
+public class IDELauncher {
+
+    // This main method is used for running the app from IntelliJ IDEA.
+    public static void main(String[] args) {
+        AttenderApp.main(args);
+    }
+}
