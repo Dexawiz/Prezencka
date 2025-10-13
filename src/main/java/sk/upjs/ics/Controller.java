bf2560afea87c131adb534a67e1005b9d5c043b7
dexawiz <nazarp118@gmail.com>
2025-10-13 13:00:03 +0200

    ...

--- src/main/java/sk/upjs/ics/Controller.java
+++ src/main/java/sk/upjs/ics/Controller.java
@@ -3,36 +3,19 @@ package sk.upjs.ics;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
-import java.io.IOException;
+import javafx.scene.control.ListView;
 
 public class Controller {
 
     @FXML
-    private Label attendanceLabel;
+    private ListView<?> attendancesListView;
 
     @FXML
-    private Button computeBtn;
+    private Button newAttendanceButton;
 
     @FXML
-    private TextField subjectTextField;
+    void newAttendanceButtonActions(ActionEvent event) {
 
-    @FXML
-    void computeBtnAction(ActionEvent event) {
-        AttendanceService service;
-        try {
-            service = AttendanceService.fromResources();
-        } catch (IOException e) {
-            attendanceLabel.setText("Error: " + e.getMessage());
-            return;
-        }
-
-        String subject = subjectTextField.getText();
-
-        var average = service.averageNumberOfStudents(subject);
-        attendanceLabel.setText("Average number of students: " + average);
     }
 
 }
